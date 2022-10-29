b35636263ebe43b309c77c700dfb4fecb5d683ad
VykDMorais <108690989+VykDMorais@users.noreply.github.com>
2022-10-29 01:12:25 -0300

    Delete quest005.py

--- Laboratory of Applied Programmation/Lista 3 de Python/quest005.py
+++
@@ -1,6 +0,0 @@
-def somaimposto (taxaimposto, custo):
-    return ((taxaimposto*custo) + custo)
-taxaimposto = float(input("Qual é a taxa de imposto? "))
-custo = int(input("Qual é o custo? "))
-print (f"O novo valor é {somaimposto(taxaimposto, custo)}")
-input()

37850d7616470b7281cbf66e04f4b34f30f867b0
VykDMorais <108690989+VykDMorais@users.noreply.github.com>
2022-10-29 01:11:41 -0300

    Delete quest002.py

--- Laboratory of Applied Programmation/Lista 3 de Python/quest002.py
+++
@@ -1,7 +0,0 @@
-def imprime (n: int):
-    for linha in range (1, n + 1):
-        for coluna in range (linha):
-            print (coluna, end='  ')
-        print ('')
-n = int(input("Digite um número: "))
-input()

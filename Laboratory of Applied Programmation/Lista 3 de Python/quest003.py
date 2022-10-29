0fe5a82fefc58c6f93871b8c327771fb19aa951a
VykDMorais <108690989+VykDMorais@users.noreply.github.com>
2022-10-29 01:11:55 -0300

    Delete quest003.py

--- Laboratory of Applied Programmation/Lista 3 de Python/quest003.py
+++
@@ -1,8 +0,0 @@
-argumento = []
-def somador (argumento):
-    soma = sum(argumento)
-    return soma
-for i in range (3):
-    argumento.append(int(input("Digite um número inteiro: ")))
-print (f"A soma desses três termos é: {somador(argumento)}")
-input()

15f183f6900050cce71c93490e1d06fc81b91f79
VykDMorais <108690989+VykDMorais@users.noreply.github.com>
2022-11-03 07:19:50 -0300

    Update and rename quest006_erro.py to quest006 - conversor de horas.py

--- Laboratory of Applied Programmation/Lista 3 de Python - Funções/quest006 - conversor de horas.py
+++ Laboratory of Applied Programmation/Lista 3 de Python - Funções/quest006 - conversor de horas.py
@@ -1,11 +1,8 @@
-#por agluma caralha, o 12 vai pra 5PM sendo q eu configurei anteriormente para ser 12PM
 def conversor (hora):
     b = hora/12
-    if hora == 12:
+    if b == 1:
         print (f"O horáio é 12:{minuto} PM")
-    if b > 1 and b < 2:
-        if hora == 12:
-            hora == 24
+    elif b > 1 and b < 2:
         print (f"O horário é {hora - 12}:{minuto} PM")
     elif b >= 0 and b < 1:
         print (f"O horário é {hora}:{minuto} AM")
@@ -14,7 +11,7 @@ def conversor (hora):
 while (True):
     hora = int(input("Qual a hora? "))
     minuto = int(input("Qual o minuto? "))
-    if minuto >= 0 and minuto < 60 and hora != 12:
+    if minuto >= 0 and minuto < 60:
         conversor (hora)
     else:
         print("Erro, número de minutos inválido")
